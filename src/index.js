b87c12eeae851d11b6f7b16e7e75bee5e1e8c43c
Lillian Namusisi <lilliannamusisi@gmail.com>
2020-09-11 09:48:48 +0300

    added home, posts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/Home';
+import Programs from './components/Programs';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
